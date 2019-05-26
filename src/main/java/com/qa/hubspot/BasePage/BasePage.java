b808e0148446810b7222bb683c23d7134d0123ba
NirmalPrasath <nirmalprasad.sep@gmail.com>
2019-05-26 20:58:30 +0530

    Aku first commit

--- src/main/java/com/qa/hubspot/BasePage/BasePage.java
+++ src/main/java/com/qa/hubspot/BasePage/BasePage.java
@@ -21,7 +21,6 @@ public class BasePage {
 		driver.manage().window().maximize();
 		//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-		
 		return driver;
 		
 	}
